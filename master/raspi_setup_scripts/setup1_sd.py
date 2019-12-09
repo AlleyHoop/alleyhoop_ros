598637c9fe5df8acc61b2cf72b0b14077034c3d0
Krebsalad <johanmgina7@gmail.com>
2019-12-09 16:50:28 +0100

    even more print messages for scripts

--- master/raspi_setup_scripts/setup1_sd.py
+++ master/raspi_setup_scripts/setup1_sd.py
@@ -52,7 +52,7 @@ interfaces_config_pth = sysarg_current_dir + "/config_files/interfaces"
 
 # user args
 userarg_target_sd = "sdb"
-userarg_target_sd_pth = "/dev" + userarg_target_sd
+userarg_target_sd_pth = "/dev/" + userarg_target_sd
 userarg_image_url = "https://ubuntu-mate.org/raspberry-pi/ubuntu-mate-16.04.2-desktop-armhf-raspberry-pi.img.xz"
 userarg_image_archive = "ubuntu-mate-16.04.2-desktop-armhf-raspberry-pi.img.xz"
 userarg_image_archive_pth = sysarg_current_dir +  "/" + userarg_image_archive
@@ -71,15 +71,17 @@ subprocess.call(["apt-get", "install", "wget"])
 
 # download image if not existing in current dir
 if not (os.path.exists(userarg_image_archive_pth)):
+    print("1. downloading " + userarg_image_archive + "...")
     subprocess.call(["wget", userarg_image_url])
     log.append("downloaded : " + userarg_image_archive_pth)
 else:
+    print("1. skipped download for " + userarg_image_archive + "...")
     log.append("did not download image " + userarg_image_archive + ", image already existing")
 
 # unarchive image
 if not (os.path.exists(userarg_image_pth)):
     if (os.path.exists(userarg_image_archive_pth)):
-        print("extracting file " + userarg_image_archive_pth)
+        print("2. extracting file " + userarg_image_archive + "... ")
         subprocess.call(["unxz", userarg_image_archive])
         log.append("unarchived file, resulting file : " + userarg_image_pth)
     else:
@@ -87,11 +89,13 @@ if not (os.path.exists(userarg_image_pth)):
         print_log()
         sys.exit()
 else:
+     print("2. skipped extraction of file " + userarg_image_archive + "... ")
      log.append("did not unarchive image " + userarg_image_archive + ", image already unarchived as " + userarg_image)
 
 # install image
 if (os.path.exists(userarg_target_sd_pth)):
     if (os.path.exists(userarg_image_pth)):
+        print("3. writing image to disk " + userarg_image_archive + "...")
         subprocess.call(["ddrescue", "-d", "-D", "--force", userarg_image, userarg_target_sd_pth])
         log.append("succesfully installed image " + userarg_image + " to " + userarg_target_sd_pth)
     else:

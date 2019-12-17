25b2bf0da01560a14c0e106c94b9abc25d25a234
krebsalad <johanmgina7@gmail.com>
2019-12-17 03:30:57 -0800

    publisher fixes

--- master/alleyhoop_master/src/alleyhoop_ros_ultrasoon.cpp
+++ master/alleyhoop_master/src/alleyhoop_ros_ultrasoon.cpp
@@ -22,7 +22,7 @@ namespace AlleyHoopROSSensors
 
     void AlleyHoopUltrasoon::callBack(const std_msgs::UInt8 msg)
     {
-        ROS_INFO("I heard: [%d]", msg.data)
+        ROS_INFO("I heard: [%d]", msg.data);
         distance = msg.data;
     }
 }

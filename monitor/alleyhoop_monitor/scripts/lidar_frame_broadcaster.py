af86009643f1927144bb0f41a9ef0508b266709b
krebsalad <johanmgina7@gmail.com>
2019-12-18 07:13:28 -0800

    lidar added in monitor demo

--- monitor/alleyhoop_monitor/scripts/lidar_frame_broadcaster.py
+++ monitor/alleyhoop_monitor/scripts/lidar_frame_broadcaster.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 import roslib
-roslib.load_manifest('test_pkg')
+roslib.load_manifest('alleyhoop_monitor')
 import rospy
 import tf
 
-rospy.init_node('vehicle_tf_broadcaster')
+rospy.init_node('lidar_tf_broadcaster')
 br = tf.TransformBroadcaster()
 rate = rospy.Rate(10.0)
 while not rospy.is_shutdown():
     br.sendTransform((0.0, 0.0, 0.0),
                      (0.0, 0.0, 0.0, 1.0),
                      rospy.Time.now(),
-                     "world",
+                     "base_link",
                      "laser")

f48625224be18881680d25207e2ab9d202015416
Krebsalad <johanmgina7@gmail.com>
2020-01-13 12:02:06 +0100

    scripts changes

--- master/raspi_setup_scripts/setup2_master_environment.py
+++ master/raspi_setup_scripts/setup2_master_environment.py
@@ -75,18 +75,22 @@ subprocess.call(["apt-get", "install", "ros-kinetic-rosserial", "-y"])
 log.append("installed rosserial libs")
 
 # install arduino IDE
-if not (os.path.exists("/tmp/arduino-1.8.10-linuxarm.tar.xz")):
-    print("downloading arduino-1.8.10-linuxarm.tar.xz")
-    subprocess.call(["wget", "https://downloads.arduino.cc/arduino-1.8.10-linuxarm.tar.xz"], cwd="/tmp/")
-    log.append("downloaded file /tmp/arduino-1.8.10-linuxarm.tar.xz")
-if not (os.path.exists("/tmp/arduino-1.8.10")):
-    print("extracting arduino-1.8.10-linuxarm.tar.xz")
-    subprocess.call(["tar", "-xf", "arduino-1.8.10-linuxarm.tar.xz"], cwd="/tmp/")
-    log.append("extracted file /tmp/arduino-1.8.10-linuxarm.tar.xz to /tmp/arduino-1.8.10")
 if not (os.path.exists("/opt/arduino-1.8.10")):
-    subprocess.call(["mv", "arduino-1.8.10", "/opt"], cwd="/tmp/")
-    subprocess.call(["/opt/arduino-1.8.10/install.sh"])
-    log.append("moved file /tmp/arduino-1.8.10 to /opt/arduino-1.8.10")
+    if not (os.path.exists("/tmp/arduino-1.8.10-linuxarm.tar.xz")):
+        print("downloading arduino-1.8.10-linuxarm.tar.xz")
+        subprocess.call(["wget", "https://downloads.arduino.cc/arduino-1.8.10-linuxarm.tar.xz"], cwd="/tmp/")
+        log.append("downloaded file /tmp/arduino-1.8.10-linuxarm.tar.xz")
+    if not (os.path.exists("/tmp/arduino-1.8.10")):
+        print("extracting arduino-1.8.10-linuxarm.tar.xz")
+        subprocess.call(["tar", "-xf", "arduino-1.8.10-linuxarm.tar.xz"], cwd="/tmp/")
+        log.append("extracted file /tmp/arduino-1.8.10-linuxarm.tar.xz to /tmp/arduino-1.8.10")
+    if not (os.path.exists("/opt/arduino-1.8.10")):
+        subprocess.call(["mv", "arduino-1.8.10", "/opt"], cwd="/tmp/")
+        subprocess.call(["/opt/arduino-1.8.10/install.sh"])
+        log.append("moved file /tmp/arduino-1.8.10 to /opt/arduino-1.8.10")
+    log.append("to use the libraries in arduino IDE run the following commands\n    a. rosrun rosserial_arduino make_libraries.py ./ && sudo mv ./ros_lib /opt/arduino-1.8.10/libraries/")
+else:
+    log.append("Did not downloa and install arduino IDE, IDE arduino-1.8.10 already installed")
 
 print_log()
 sys.exit()
